de559e51a709e7e50e9b5adec268b1a22672fab3
sociengineer <itct06@itct06ui-iMac.local>
2015-11-27 12:52:44 +0900

    Project Init

---
+++ JaeHeung/Pineco/Pineco/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Pineco
+//
+//  Created by itct06 on 2015. 11. 27..
+//  Copyright © 2015년 itct. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
